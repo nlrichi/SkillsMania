3b56a33fc5772a6f101068a6aad27964ed2e9b1e
sf361 <sf361@student.le.ac.uk>
2024-02-13 17:24:17 +0000

    Testing Avatar Display with user

---
+++ java_mvc_base/src/main/java/org/example/java_mvc_base/model/User.java
@@ -0,0 +1,50 @@
+package org.example.java_mvc_base.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class User {
+    @Id
+    private String username;
+//    @ManyToOne
+//    private LeagueTable league;
+
+    private int currentStreak;
+
+    private int overallXp;
+
+//    public LeagueTable getLeague() {
+//        return league;
+//    }
+//
+//    public void setLeague(LeagueTable league) {
+//        this.league = league;
+//    }
+
+    public int getCurrentStreak() {
+        return currentStreak;
+    }
+
+    public void setCurrentStreak(int currentStreak) {
+        this.currentStreak = currentStreak;
+    }
+
+    public int getOverallXp() {
+        return overallXp;
+    }
+
+    public void setOverallXp(int overallXp) {
+        this.overallXp = overallXp;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
