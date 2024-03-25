fae797e8ae8da0ed638b78af7fa426c32447b363
rya3 <rya3@student.le.ac.uk>
2024-03-25 22:50:57 +0000

    Coin reward system final

--- java_mvc_base/src/main/java/org/example/java_mvc_base/controller/DurationController.java
+++ java_mvc_base/src/main/java/org/example/java_mvc_base/controller/DurationController.java
@@ -169,10 +169,10 @@ public class DurationController {
         loggedInUser.setCompletedCourses(completedCourses);
         //Add 50 coins to the user's total coins everytime they complete a course
         loggedInUser.setTotalCoins(loggedInUser.getTotalCoins() + 50);
-        //Update the model to include total coins
-        model.addAttribute("totalCoin", loggedInUser.getTotalCoins());
         // save the users info in the repo
         userRepository.save(loggedInUser);
+        //Update the model to include total coins
+        model.addAttribute("totalCoin", loggedInUser.getTotalCoins());
         System.out.println("COINS GIVEN. NOW EQUALS " +loggedInUser.getTotalCoins());
 
 
